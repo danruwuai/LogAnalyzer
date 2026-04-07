ed457859b29f34e43ccbfa5da971d458862bae94
danruwuai <danruwuai@gmail.com>
2026-04-07 18:33:40 +0800

    chore: 更新.gitignore，移除不必要的文件

--- create-cert.ps1
+++
@@ -1,2 +0,0 @@
-$pwd = ConvertTo-SecureString -String "LogAnalyzer123" -Force -AsPlainText
-Export-PfxCertificate -Cert "Cert:\CurrentUser\My\2FB37E92E3947EF38172EB5BBBB6DD3CB2709B68" -FilePath "C:\Users\zgj\.openclaw\workspace\LogAnalyzer\codesign.pfx" -Password $pwd
